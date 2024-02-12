8a251d6b7407d5654fecdea7097a4e9ee8836bf7
momen210 <touristmomen@gmail.com>
2024-02-12 23:01:51 +0600

    adding vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 // mongo code
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t6peb0q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
